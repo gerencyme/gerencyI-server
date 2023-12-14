2a47942bc8327872e721bed0de8ed01cfa2ed412
joao vitor <38108009+joaoguthix@users.noreply.github.com>
2023-12-14 00:26:06 -0300

    Update Program.cs

--- GerencylApiAuthentication/ApiAuthentication/Program.cs
+++ GerencylApiAuthentication/ApiAuthentication/Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Gerencyl-Authentication", Version = "v1" });
 
-    // Configuração para autenticação com Bearer Token
+    // ConfiguraÃ§Ã£o para autenticaÃ§Ã£o com Bearer Token
     var securityScheme = new OpenApiSecurityScheme
     {
         Name = "Authorization",
@@ -106,7 +106,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 //SENDGRID
 
 builder.Services.AddSendGrid(options =>
-{
+
     options.ApiKey = builder.Configuration
     .GetSection("SendGridEmailSettings").GetValue<string>("APIKey");
 });
