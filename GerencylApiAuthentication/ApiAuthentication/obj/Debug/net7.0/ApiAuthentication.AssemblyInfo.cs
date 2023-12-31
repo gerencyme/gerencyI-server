28215eadd25f8d6c8c933910b70b22e706e7cd5c
joao rossetto <guerreiroxx8@gmail.com>
2023-12-30 21:26:53 -0300

    ah nem lembro foi muita coisa

--- GerencylApiAuthentication/ApiAuthentication/obj/Debug/net7.0/ApiAuthentication.AssemblyInfo.cs
+++ GerencylApiAuthentication/ApiAuthentication/obj/Debug/net7.0/ApiAuthentication.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiAuthentication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e0dd70cd7ad5fe19de5b5f8b299d8e2bfaa5d3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9d67846af767df137c8011c4a5f09dadd65dffb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiAuthentication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiAuthentication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
