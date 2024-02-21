3b1fa1b6cb60102964c32d0bd390984744bbd4db
joao rossetto <guerreiroxx8@gmail.com>
2024-02-21 12:09:09 -0300

    v2 gerencyi

--- GerencylApiAuthentication/ApiAuthentication/Program.cs
+++ GerencylApiAuthentication/ApiAuthentication/Program.cs
@@ -175,7 +175,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "GerencyI v1");
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "GerencyI v2");
     });
 }
 else app.MapControllers();
